37fe595562a738da91bf44f7c7b1281a603b4bdf
Nathan Contino <WinterMute@dhcp-10-5-13-217.wireless.rochester.edu>
2016-04-20 08:11:13 -0400

    functional site
    (fingers crossed)

--- Archive/AllCourses_files/ErrorHandling.js
+++
@@ -1,9 +0,0 @@
-﻿// JScript File
-
-function closeErrorWindow() {
-    window.close();
-}
-
-function openErrorWindow(ID) {
-    window.open('ErrorHandler.aspx?ID=' + ID, 'ERRORWINDOW', 'alwaysRaised=yes,toolbar=no,width=535,height=350,resizable=yes,scrollbars=yes');
-}
